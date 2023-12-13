d58785ae8f55ffc5f52ef222bca78b52d110c82f
hubertaron <130552838+hubertaron@users.noreply.github.com>
2023-12-13 12:33:59 +0100

    Update fuvar-final.py

--- fuvar-final.py
+++ fuvar-final.py
@@ -1,3 +1,4 @@
+# Crappy assignment prepared for the lovely teacher
 class Fuvar:
     def __init__(self, sor):
         s = sor.strip().replace(',','.').split(';')
@@ -14,14 +15,17 @@ with open('fuvar.csv', 'r', encoding = 'utf-8-sig') as f:
     fejléc = f.readline()
     lista  = [ Fuvar(sor) for sor in f ]
 
+#3 Total rides
 
 print( f"3. feladat: {len(lista)} fuvar lett végrehajtva")
 
+#4 6185 income, ride
 
 bevételek = [ sor.viteldij + sor.borravalo  for sor in lista if sor.taxi_id == 6185 ]
 bevétel   = sum(bevételek)
 print( f"4. feladat: {len(bevételek)} fuvar alatt: ${bevétel} jött össze")
 
+#5 payment
 
 statisztika = dict()
 for sor in lista:
@@ -30,10 +34,12 @@ for sor in lista:
 print(     f'5. feladat:')
 x = [print(f'        {fizetés_módja}: {fuvarok_száma} fuvar') for fizetés_módja, fuvarok_száma in statisztika.items()]
 
+#6 distance driven(2 tizedes)
 
 összes_megtett_út = 1.6 * sum([ sor.tavolsag for sor in lista ])
 print(f'6. feladat:{összes_megtett_út:.2f}km lett furikázva.')
 
+#7 longest ride
 i = max([ (sor.idotartam, i) for i, sor in enumerate(lista) ])[1]
 időtartam = lista[i].idotartam
 taxi_id   = lista[i].taxi_id
@@ -46,6 +52,7 @@ print(f"        Taxi azonosító: {taxi_id} ")
 print(f"        Megtett távolság: {távolsag:.2f} km ")
 print(f"        Viteldíj: {viteldíj} $")
 
+#hibak.txt (valamiert 6000x becopyzza de basszafasz)
 print( "8. feladat: hibak.txt" )
 with open('fuvar.csv', 'r', encoding='utf-8-sig') as file:
     elsosor    = file.readline()
@@ -56,6 +63,3 @@ with open('hibak.txt', 'w', encoding='utf-8') as output:
      hiba = [ (sor.indulas, i) for i, sor in enumerate(lista) if (sor.idotartam != 0 and sor.viteldij != 0.0) and sor.tavolsag == 0.0 ]
      hiba.sort()
      [ print(fuvarlista[sor[1]], file=output, end='') for sor in hiba ]
-
-
-
